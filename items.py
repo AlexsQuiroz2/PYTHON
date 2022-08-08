6bb0631ff2383901c7e276202006ede6cfe0a5bd
alexs <alexsquiroz054@gmail.com>
2022-08-08 15:22:41 -0500

    cambio de parametros

--- items.py
+++ items.py
@@ -10,23 +10,39 @@ print(type(items_total_price)) # int '''
 
 
 class item:
-    def calculate_total_price(self, x, y):
-         return x * y
+    def __init__(self, name, price, quantity=0):
+         print("An instance created: {name}")
+         self.name = name 
+         self.price = price
+         self.quantity = quantity
+    
+    def calculate_total_price(self):
+         return self.price * self.quantity
 
-item1 = item()
-item1.name = "Phone"
-item1.price = 100
-item1.quantity = 5
-item1.calculate_total_price(item1.price, item1.quantity)
-print(item1.calculate_total_price(item1.price, item1.quantity))
+item1 = item("phone", 100, 1)
+'''item1.name = "Phone"'''
+'''item1.price = 100'''
+'''item1.quantity = 5'''
+'''item1.calculate_total_price(item1.price, item1.quantity)
+print(item1.calculate_total_price(item1.price, item1.quantity))'''
+    
+item2 = item("laptop", 1000, 3)
+'''item2.name = "Laptop"'''
+'''item2.price = 1000
+item2.quantity = 3'''
+'''print(item2.calculate_total_price(item2.price, item2.quantiy))'''
 
-item2 = item()
-item2.name = "Laptop"
-item2.price = 1000
-item2.quantity = 3
+print(item1.calculate_total_price())
+print(item2.calculate_total_price())
 
 
- 
+
+'''print(item1.name)
+print(item2.name)
+print(item1.price)
+print(item2.price)'''
+'''print(item1.quantity)
+print(item2.quantity)'''
 
 
 
