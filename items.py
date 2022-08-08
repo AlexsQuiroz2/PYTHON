64cfd5ed9b4b6ca9f9a1616b145b07829294c299
alexs <alexsquiroz054@gmail.com>
2022-08-08 17:27:04 -0500

    version 1.2

--- items.py
+++ items.py
@@ -11,28 +11,32 @@ print(type(items_total_price)) # int '''
 
 class item:
     pay_rate = 0.8 #The pay rate after 20% discount
+    all = []
     def __init__(self, name: str, price: float, quantity=0):        
         assert price >= 0, f"price {price} is not greater than or equal to zero"
         assert quantity >= 0, f"quantity {quantity} is not greater than or equal to zero"
         
+        # Assign to self object
         self.name = name 
         self.price = price
         self.quantity = quantity
         
+        # Actions to execute
+        item.all.append(self)
     
     def calculate_total_price(self):
          return self.price * self.quantity
      
     def apply_discount(self):
         self.price = self.price * self.pay_rate
+    
+    def __repr__(self):
+        return f"item('{self.name}', {self.price}, {self.quantity})"
 
-item1 = item("phone", 100, 1) 
-item1.apply_discount()
-print(item1.price)
-
+item1 = item("phone", 100, 1)
 item2 = item("laptop", 1000, 3)
-item2.pay_rate = 0.7
-item2.apply_discount()
-print(item2.price)
+item3 = item("cable", 10, 5)
+item4 = item("mouse", 50, 5)
+item5 = item("keyboard", 75, 5)
 
-
+print(item.all)
